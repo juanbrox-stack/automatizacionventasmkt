eb5fe8fb47ffd30825949bc2cf88bb6aaaba0a23
juanbrox-stack <juanbrox@cecotec.es>
2026-05-20 11:42:10 +0200

    Change email recipient from martacuesta to raulmartinez

--- automatizacionventasmkt.py
+++ automatizacionventasmkt.py
@@ -89,7 +89,7 @@ def enviar_correo_marta(excel_bytes, nombre_archivo, rango_fechas):
         smtp_server = st.secrets["correo"]["servidor_smtp"]
         puerto = int(st.secrets["correo"]["puerto"])
         
-        destinatario = "martacuesta@cecotec.es"
+        destinatario = "raulmartinez@cecotec.es"
         
         # Configurar la estructura del correo electrónico
         msg = MIMEMultipart()
